90b4f7728c283a9c2d31c8f9ef590eec34d231b4
happy_sadan <tristancottam.1@gmail.com>
2021-06-21 14:59:49 +0200

    fix: Fix memory leaks and out-of-range writes

--- src/arg_to_conv/char_arg_to_conv.c
+++ src/arg_to_conv/char_arg_to_conv.c
@@ -6,7 +6,7 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 15:32:14 by trcottam          #+#    #+#             */
-/*   Updated: 2020/12/09 15:51:52 by trcottam         ###   ########.fr       */
+/*   Updated: 2021/06/21 11:51:25 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	char_arg_to_conv(va_list *args, t_conv *conv)
 {
 	conv->var.c = va_arg(*args, int);
-	conv->str = ft_strdup(ft_chartostr(conv->var.c));
+	conv->str = ft_chartostr(conv->var.c);
 }
--- src/print_null_char_conv.c
+++ src/print_null_char_conv.c
@@ -6,7 +6,7 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 16:50:46 by trcottam          #+#    #+#             */
-/*   Updated: 2020/12/09 13:14:00 by trcottam         ###   ########.fr       */
+/*   Updated: 2021/06/21 11:57:50 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	print_null_char_conv(t_conv *conv)
 {
-	conv->str[ft_strlen(conv->str) - 1] = '\0';
+	if (conv->str[0])
+		conv->str[ft_strlen(conv->str) - 1] = '\0';
 	if (conv->left)
 		ft_putchar_fd('\0', 1);
 	ft_putstr_fd(conv->str, 1);
